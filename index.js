8bb3213b9409c132d9b851782a3519980ab582cc
infinity-vendas <github.script.oficial@gmail.com>
2026-04-18 00:41:22 -0300

    index.js

--- index.js
+++ index.js
@@ -1,12 +1,23 @@
 const TelegramBot = require("node-telegram-bot-api");
+const express = require("express");
 
-const bot = new TelegramBot("8227400926:AAF5sWBB6n63wZueUo_XQBVSgs6lBGLsAiE", {
-    polling: {
-        interval: 1000,
-        autoStart: true
-    }
+const app = express();
+
+// TOKEN
+const bot = new TelegramBot("8227400926:AAF5sWBB6n63wZueUo_XQBVSgs6lBGLsAiE", { polling: true });
+
+// START simples e seguro
+bot.onText(/\/start/, (msg) => {
+    bot.sendMessage(msg.chat.id, "🤖 Bot online funcionando!");
 });
 
-bot.onText(/\/start/, (msg) => {
-    bot.sendMessage(msg.chat.id, "Bot online 🚀");
+// servidor obrigatório pro Render
+app.get("/", (req, res) => {
+    res.send("Bot rodando 🚀");
+});
+
+const PORT = process.env.PORT || 3000;
+app.listen(PORT, () => {
+    console.log("Servidor ativo na porta " + PORT);
+    console.log("Bot iniciado");
 });
