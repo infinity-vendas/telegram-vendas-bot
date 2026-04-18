fb5cab347c1a76b366900367849e03147b7f09e9
infinity-vendas <github.script.oficial@gmail.com>
2026-04-18 00:07:42 -0300

    index.js

--- index.js
+++ index.js
@@ -1,6 +1,11 @@
 const TelegramBot = require("node-telegram-bot-api");
 
-const bot = new TelegramBot("8227400926:AAF5sWBB6n63wZueUo_XQBVSgs6lBGLsAiE", { polling: true });
+const bot = new TelegramBot("8227400926:AAF5sWBB6n63wZueUo_XQBVSgs6lBGLsAiE", {
+    polling: {
+        interval: 1000,
+        autoStart: true
+    }
+});
 
 bot.onText(/\/start/, (msg) => {
     bot.sendMessage(msg.chat.id, "Bot online 🚀");
